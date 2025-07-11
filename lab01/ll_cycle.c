e94ac96a6bebdb510fee0b4dc4bb06b83ffbf9bc
Shadoxx <shadoxx152@gmail.com>
2025-07-11 20:16:05 +0800

    lab1 and proj1

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,23 @@
-#include <stddef.h>
 #include "ll_cycle.h"
+#include <stddef.h>
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+	if (head == NULL || head->next == NULL) {
+		return 0; 
+	}
+
+	struct node *tortoise = head;
+    struct node *hare = head;
+
+  	while (hare->next != NULL && hare->next->next != NULL) {
+		tortoise = tortoise->next; 
+		hare = hare->next->next;
+		
+		if (tortoise == hare) {
+			return 1; // Cycle detected
+		}
+	}
+	
+	// If we reach here, no cycle was detected
+	return 0;
 }
